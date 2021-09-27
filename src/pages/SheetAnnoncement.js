96562ea75facef5bde72c2dd566565d96d86e31c
ChrisP <petitcolaschristoper@gmail.com>
2021-09-27 20:59:15 +0200

    Ajout d'un effet de bord pour changer les images des annonces et amélioration du style

--- src/pages/SheetAnnoncement.js
+++ src/pages/SheetAnnoncement.js
@@ -3,14 +3,19 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-import "../styles/annoncement.css";
 import Information from "../components/Information";
 import HeadingAnnoncement from "../components/HeadingAnnoncement";
 import Owner from "../components/Owner";
 import Tags from "../components/Tags";
 import Stars from "../components/Stars";
 
+import "../styles/annoncement.css";
+
 class SheetAnnoncement extends Component {
+  state = {
+    index: 0,
+  };
+
   render() {
     const currentData = this.props.location.params;
 
@@ -18,7 +23,15 @@ class SheetAnnoncement extends Component {
       <div className="sheet-annoncement">
         <Header />
         <div className="bannier-house">
-          <img className="picture-house" src={currentData.cover} alt="bannière de l'annonce" />
+          <span
+            className="fas fa-chevron-left"
+            onClick={() => this.setState({ index: this.state.index === 0 ? this.state.index : this.state.index - 1 })}
+          ></span>
+          <img className="picture-house" src={currentData.pictures[this.state.index]} alt="bannière de l'annonce" />
+          <span
+            className="fas fa-chevron-right"
+            onClick={() => this.setState({ index: this.state.index === currentData.pictures.length - 1 ? this.state.index : this.state.index + 1 })}
+          ></span>
         </div>
         <div className="container-annoncement">
           <div className="main-info">
@@ -26,15 +39,15 @@ class SheetAnnoncement extends Component {
             <Owner nameOwner={currentData.host.name} picture={currentData.host.picture} />
           </div>
           <div className="secondary-info">
-            <Tags tag={currentData.tags} /> 
+            <Tags tag={currentData.tags} />
             <Stars rating={currentData.rating} />
           </div>
           <div className="blocs-info">
             <Information detail={currentData.description} title={"Description"} index={0} />
             <Information atout={currentData.equipments} title={"Equipement"} index={1} />
           </div>
-          <Footer />
         </div>
+        <Footer />
       </div>
     );
   }
